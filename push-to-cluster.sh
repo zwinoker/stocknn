d3796bbe09271786de8e84c995a85916e84b880e
Zachary Winoker <zacharywinoker@Zacharys-MacBook-Pro.local>
2016-05-07 19:30:35 -0700

    server stuff

--- push-to-cluster.sh
+++ push-to-cluster.sh
@@ -2,10 +2,16 @@ starcluster put smallcluster ./* ./root
 
 for i in `seq 1 9`;
 	do
-		echo `starcluster put --node node00$i ./* ./root`
+		echo `yes | starcluster sshnode smallcluster node00$i`
+		echo `yes | mkdir data network`
+		logout
+		echo `yes | starcluster put --node node00$i ./* ./`
 	done 
 
 for i in `seq 10 15`;
 	do
-		echo `starcluster put --node node0$i ./* ./root`
+		echo `yes | starcluster sshnode smallcluster node0$i`
+		echo `yes | mkdir data network`
+		logout
+		echo `yes | starcluster put --node node0$i ./* ./root`
 	done 
